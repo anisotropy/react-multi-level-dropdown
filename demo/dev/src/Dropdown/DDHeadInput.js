9dfabdcc01c319d3ac712b150febca48c26cb21b
hodoug <j.hodoug@gmail.com>
2017-11-06 02:31:53 +0900

    Add auto-size-input and change style

--- demo/dev/src/Dropdown/DDHeadInput.js
+++ demo/dev/src/Dropdown/DDHeadInput.js
@@ -1,20 +1,26 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import DDHeadLabel from './DDHeadLabel';
+import AutosizeInput from 'react-input-autosize';
 import h_ from './helpful';
 import './ddheadinput.less';
 
 class DDHeadInput extends PureComponent {
   constructor(props){
     super(props);
+    this.state = {keyword: ''};
     this.handleClickArrow = this.handleClickArrow.bind(this);
     this.handleClickLabel = this.handleClickLabel.bind(this);
+    this.handleChangeKeyword = this.handleChangeKeyword.bind(this);
   }
   handleClickArrow(){
     this.props.onClickArrow();
   }
   handleClickLabel(value){
     this.props.onClickLabel(value);
+  }
+  handleChangeKeyword(e){
+    this.setState({keyword: e.target.value});
   }
   findLabel(data, value){
     return h_.extract(data, (d) => (
@@ -23,6 +29,7 @@ class DDHeadInput extends PureComponent {
   }
   render(){
     const {type, data, selected, open} = this.props;
+    const {keyword} = this.state;
     const labels = selected.map((value) => (
       <DDHeadLabel key={value}
         value={value}
@@ -30,12 +37,20 @@ class DDHeadInput extends PureComponent {
         onClick={this.handleClickLabel}
       />
     ));
+    const input = (
+      <AutosizeInput
+        className="ddheadinput__input"
+        value={keyword}
+        style={{minWidth: '5em'}}
+        onChange={this.handleChangeKeyword}
+      />
+    );
     return (
       <div className="ddheadinput">
         <div className={h_.class('ddheadinput__arrow', [{open}])} onClick={this.handleClickArrow}></div>
         <div className="ddheadinput__labels">
           {labels}
-          <input />
+          {input}
         </div>
       </div>
     );
