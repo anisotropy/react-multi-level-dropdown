ad99d340f81518f2afb3c249c8474a9dbb8eef28
hodoug <j.hodoug@gmail.com>
2017-11-07 03:07:29 +0900

    Move focus and select by pressing keys

--- demo/dev/src/Dropdown/DDItem.js
+++ demo/dev/src/Dropdown/DDItem.js
@@ -11,10 +11,13 @@ class DDItem extends PureComponent {
       open: false
     };
     this.setRef = this.setRef.bind(this);
+    this.setRefLabel = this.setRefLabel.bind(this);
+    this.setRefArrow = this.setRefArrow.bind(this);
     this.handleClickArrow = this.handleClickArrow.bind(this);
     this.handleClickLabel = this.handleClickLabel.bind(this);
     this.handleBodyClick = this.handleBodyClick.bind(this);
     this.handleClickChildLabel = this.handleClickChildLabel.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
   componentDidMount(){
     document.addEventListener('click', this.handleBodyClick);
@@ -25,11 +28,38 @@ class DDItem extends PureComponent {
   setRef(node){
     this.ref = node;
   }
-  handleClickArrow(e){
+  setRefLabel(node){
+    this.refLabel = node;
+  }
+  setRefArrow(node){
+    this.refArrow = node;
+  }
+  handleClickArrow(){
     this.setState({open: !this.state.open});
   }
   handleClickLabel(){
     this.props.onClickLabel(this.props.data.value);
+  }
+  handleKeyDown(e){
+    switch(e.key){
+      case 'Enter':
+        this.ref.getElementsByClassName('dditem__area-of-label')[0].click(); break;
+      case 'ArrowRight': if(this.props.data.children){
+        if(!this.state.open){
+          this.ref.getElementsByClassName('dditem__arrow')[0].click();
+        } else {
+          this.ref.getElementsByClassName('dditem__children')[0].children[0].children[0].focus();
+        }} break;
+      case 'ArrowLeft': {
+        let parent = this.ref.parentElement.parentElement.parentElement.children[0];
+        if(parent) parent.focus();
+        if(this.state.open) this.ref.getElementsByClassName('dditem__arrow')[0].click();
+      } break;
+      case 'ArrowDown':
+        if(this.ref.nextSibling) this.ref.nextSibling.children[0].focus(); break;
+      case 'ArrowUp':
+        if(this.ref.previousSibling) this.ref.previousSibling.children[0].focus(); break;
+    }
   }
   handleClickChildLabel(value){
     this.props.onClickLabel(value);
@@ -44,12 +74,13 @@ class DDItem extends PureComponent {
     const {open} = this.state;
     const isSelected = (type == 'multi-select' ? selected.indexOf(data.value) >= 0 : selected == data.value);
     const body = (
-      <div className="dditem__body">
+      <div className="dditem__body" tabIndex="0" onKeyDown={this.handleKeyDown}>
         <span className="dditem__label">{data.label}</span>
         <div className={isSelected ? 'dditem__check' : null}></div>
         <div className="dditem__area-of-label" onClick={this.handleClickLabel}></div>
         {data.children &&
-          <div className={'dditem__arrow' + (open ? ' dditem__arrow--rotate' : '')}
+          <div
+            className={'dditem__arrow' + (open ? ' dditem__arrow--rotate' : '')}
             onClick={this.handleClickArrow}>
           </div>
         }
@@ -64,7 +95,7 @@ class DDItem extends PureComponent {
       />
     )));
     return (
-      <div ref={this.setRef} className="dditem">
+      <div className="dditem" ref={this.setRef}>
         {body}
         <div className="dditem__children-wrapper">
           <div className={'dditem__children' + (open ? ' dditem__children--open' : '')}>
